15aac891c69f828394131bfb6e7fb6d4acc628d8
aluno <aluno@172.16.3.25>
2022-12-06 16:14:15 -0300

    load

--- ProjetoJava/src/Enderco.java
+++ ProjetoJava/src/Enderco.java
@@ -1,4 +1,40 @@
 
 public class Enderco {
- jjjj 
+	private String rua;
+	private int numero;
+	private String cep;
+	private String cidade;
+	private String estado;
+	//sss
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
 }
