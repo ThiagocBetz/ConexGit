c5fabb79e595571ef2c636928e75c78580973d2e
aluno <aluno@172.16.3.28>
2022-12-13 14:19:42 -0300

    batatta

--- ProjetoJava/src/Cliente.java
+++ ProjetoJava/src/Cliente.java
@@ -1,4 +1,33 @@
 
-public class Cliente {
-
+public class Cliente extends Enderco {
+	private int clienteid;
+	private String nome;
+	private Enderco enderco;
+	
+	public int getClienteid() {
+		return clienteid;
+	}
+	public void setClienteid(int clienteid) {
+		this.clienteid = clienteid;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Enderco getEnderco() {
+		return enderco;
+	}
+	public void setEnderco(Enderco enderco) {
+		this.enderco = enderco;
+	}
+	public String toString() {
+		String texto = "";
+		
+		texto = "------------------"
+				+ "\n cliente id: " + getClienteid()
+				+ "\n numero: " + getNome();
+		return texto;
+	}
 }
