cdf0a6d7c1d136f37ca7645204adc9b8e98f9d37
aluno <aluno@172.16.3.20>
2022-12-20 16:38:45 -0300

    tafira

---
+++ ProjetoJava/src/Tarifa.java
@@ -0,0 +1,49 @@
+
+public class Tarifa {
+	private double tarResidencia1 = 0.38790;
+	private double tarResidencia2 = 0.40500;
+	private double tarComercial1 = 0.42550;
+	private double tarComercial2 = 0.45113;
+	private double icms = 27;
+	private double taxailuminacaoPublica = 7.8;
+	
+	public double getTarResidencia1() {
+		return tarResidencia1;
+	}
+	public void setTarResidencia1(double tarResidencia1) {
+		this.tarResidencia1 = tarResidencia1;
+	}
+	public double getTarResidencia2() {
+		return tarResidencia2;
+	}
+	public void setTarResidencia2(double tarResidencia2) {
+		this.tarResidencia2 = tarResidencia2;
+	}
+	public double getTarComercial1() {
+		return tarComercial1;
+	}
+	public void setTarComercial1(double tarComercial1) {
+		this.tarComercial1 = tarComercial1;
+	}
+	public double getTarComercial2() {
+		return tarComercial2;
+	}
+	public void setTarComercial2(double tarComercial2) {
+		this.tarComercial2 = tarComercial2;
+	}
+	public double getIcms() {
+		return icms;
+	}
+	public void setIcms(double icms) {
+		this.icms = icms;
+	}
+	public double getTaxailuminacaoPublica() {
+		return taxailuminacaoPublica;
+	}
+	public void setTaxailuminacaoPublica(double taxailuminacaoPublica) {
+		this.taxailuminacaoPublica = taxailuminacaoPublica;
+	}
+	
+	
+
+}
